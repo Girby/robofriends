ee6b6db3953e270c564c8b1eaf910bb9f32f0f7e
Girby <cstval@googlemail.com>
2021-03-09 00:26:50 +0000

    hook implementation

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,31 +1,24 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import CardList from "../components/CardList";
 import SearchBox from "../components/SearchBox";
 import Scroll from '../components/Scroll';
 import ErrorBoundary from '../components/ErrorBoundary';
 import './App.css';
 
-class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      robots: [],
-      searchField: "",
-    };
-  }
+function App() {
+  const [robots, setRobots] = useState([]);
+  const [searchField, setSearchField] = useState("");
 
-  componentDidMount(){
+  useEffect(() => {
     fetch('https://jsonplaceholder.typicode.com/users')
     .then(res => res.json())
-    .then(users => this.setState({robots: users}));
+    .then(users => setRobots(users));
+  }, [])
+
+  const searchChange = (event) => {
+    setSearchField(event.target.value)
   }
 
-  searchChange = (event) =>{
-    this.setState({searchField: event.target.value});
-  }
-
-  render() {
-    const { robots, searchField } = this.state;
     const filteredRobots = robots.filter(robot => {
         return robot.name.toLowerCase().includes(searchField.toLowerCase());
     });
@@ -35,7 +28,7 @@ class App extends React.Component {
       (
         <div className="tc">
           <h1 className="f1" >RoboFriends</h1>
-          <SearchBox searchChange={this.searchChange}/>
+          <SearchBox searchChange={searchChange}/>
           
           <Scroll>
             <ErrorBoundary>
@@ -44,7 +37,6 @@ class App extends React.Component {
           </Scroll>
         </div>
       );
-  }
 }
 
 export default App;
